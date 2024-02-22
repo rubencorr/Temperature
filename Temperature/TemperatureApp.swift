43faf1a303225d77f2f1b12982b2cecb13f25d3f
ruben_corr <rubencorr@users.noreply.github.com>
2024-02-22 19:09:00 +0100

    Initial Commit

---
+++ Temperature/TemperatureApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemperatureApp.swift
+//  Temperature
+//
+//  Created by Rúben Correia on 22.02.24.
+//
+
+import SwiftUI
+
+@main
+struct TemperatureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
